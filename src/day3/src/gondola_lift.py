cf2f3176cb08499574e701cb79c46a6ff7723607
Hinal Jogani <hinaljogani02@gmail.com>
2023-12-03 17:05:50 +0100

    AOC Day 3 Solution Part 2 added.

--- src/day3/src/gondola_lift.py
+++ src/day3/src/gondola_lift.py
@@ -1,40 +1,86 @@
 import re
 
+
 def is_symbol(s):
     return s != "." and not s.isdigit() and not s == '\n'
 
+
 def find_valid_part_number(engine_schematic, symbol_row, symbol_column):
     valid_part_num = {}
-    for row_num in [symbol_row-1, symbol_row, symbol_row+1]:
+    for row_num in [symbol_row - 1, symbol_row, symbol_row + 1]:
         if len(engine_schematic) > row_num >= 0:
             reading_row = engine_schematic[row_num]
             part_nums = re.findall(r'\d+', reading_row)
-            for part_num in part_nums:
+            for part_num in set(part_nums):
                 for match in re.finditer(part_num, reading_row):
-                    if not reading_row[match.end()].isdigit() and not reading_row[match.start()-1].isdigit() and (match.end()-1 == symbol_column-1 or match.end()-1 == symbol_column or match.end()-1 == symbol_column+1):
-                        valid_part_num.update({(row_num, match.start()): int(part_num)})
-                    elif not reading_row[match.end()].isdigit() and not reading_row[match.start()-1].isdigit() and (match.start() == symbol_column-1 or match.start() == symbol_column or match.start() == symbol_column+1):
-                        valid_part_num.update({(row_num, match.start()): int(part_num)})
+                    # Need to remove the ones that have false search like searching 3 in ..3...234.. should result only
+                    # one and not two entries
+                    if not reading_row[match.end()].isdigit() and not reading_row[match.start() - 1].isdigit():
+                        if match.end() - 1 == symbol_column - 1 or match.end() - 1 == symbol_column or match.end() - 1 == symbol_column + 1:
+                            valid_part_num.update({(row_num, match.start()): int(part_num)})
+                        elif match.start() == symbol_column - 1 or match.start() == symbol_column or match.start() == symbol_column + 1:
+                            valid_part_num.update({(row_num, match.start()): int(part_num)})
 
     return valid_part_num
 
 
 def get_missing_part_number(engine_schematic):
     valid_part_num = {}
-    symbols = []
+
     for r, line in enumerate(engine_schematic):
         for c, l in enumerate(list(line)):
             if is_symbol(l):
-                symbols.append(l)
                 valid_part_num.update(find_valid_part_number(engine_schematic, r, c))
 
     return valid_part_num.values()
 
+
+def find_gear_ratio_of_symbol(engine_schematic, symbol_row, symbol_column, valid_gear, symbol):
+    valid_part_num = []
+
+    if not symbol == '*':
+        return 0
+
+    for row_num in [symbol_row - 1, symbol_row, symbol_row + 1]:
+        if len(engine_schematic) > row_num >= 0:
+            reading_row = engine_schematic[row_num]
+            part_nums = re.findall(r'\d+', reading_row)
+            for part_num in set(part_nums):
+                for match in re.finditer(part_num, reading_row):
+                    # Need to remove the ones that have false search like searching 3 in ..3...234.. should result only
+                    # one and not two entries
+                    if not reading_row[match.end()].isdigit() and not reading_row[match.start() - 1].isdigit():
+                        if match.end() - 1 == symbol_column - 1 or match.end() - 1 == symbol_column or match.end() - 1 == symbol_column + 1:
+                            valid_part_num.append(int(part_num))
+                        elif match.start() == symbol_column - 1 or match.start() == symbol_column or match.start() == symbol_column + 1:
+                            valid_part_num.append(int(part_num))
+
+    if len(valid_part_num) == valid_gear:
+        return valid_part_num[0]*valid_part_num[1]
+    else:
+        return 0
+
+
+def get_gear_ratio(engine_schematic, valid_gear):
+    sum_of_gear_ratios = 0
+
+    for r, line in enumerate(engine_schematic):
+        for c, l in enumerate(list(line)):
+            if is_symbol(l):
+                sum_of_gear_ratios += find_gear_ratio_of_symbol(engine_schematic, r, c, valid_gear, l)
+
+    return sum_of_gear_ratios
 
 if __name__ == "__main__":
     input = open(r"day3.txt")
     missing_part_number = get_missing_part_number(input.readlines())
     input.close()
 
+    input = open(r"day3.txt")
+    valid_gear_value = 2  # Part Number
+    sum_of_gear_ratios = get_gear_ratio(input.readlines(), valid_gear_value)
+    input.close()
+
     print("Day 3:")
     print("\tPart 1: ", sum(missing_part_number))
+    print("\tPart 2: ", sum_of_gear_ratios)
--- src/day3/test/test_gonola_lift.py
+++ src/day3/test/test_gonola_lift.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day3.src.gondola_lift import get_missing_part_number
+from day3.src.gondola_lift import get_missing_part_number, get_gear_ratio
 
 
 class Test_Cube_Games(unittest.TestCase):
@@ -14,6 +14,18 @@ class Test_Cube_Games(unittest.TestCase):
         test_input2 = open("part1Test2.txt")
         missing_part_number = get_missing_part_number(test_input2.readlines())
         self.assertEqual(sum(missing_part_number), 237, "Should be 237")
+        test_input2.close()
+
+    def test_get_gear_ratio(self):
+        test_input = open("day3Part1TestInput.txt")
+        sum_of_gear_ratios = get_gear_ratio(test_input.readlines(), 2)
+        self.assertEqual(sum_of_gear_ratios, 467835, "Should be 467835")
+        test_input.close()
+
+    def test_get_gear_ratio2(self):
+        test_input2 = open("part2Test2.txt")
+        sum_of_gear_ratios = get_gear_ratio(test_input2.readlines(), 2)
+        self.assertEqual(sum_of_gear_ratios, 343742, "Should be 343742")
         test_input2.close()
 
 if __name__ == '__main__':
