2f99c44acbadb9fc8750db6c9e70d579077e2bef
Hinal Jogani <hinaljogani02@gmail.com>
2023-12-10 14:52:53 +0100

    AOC Day 9 Solution optimized.

--- src/day9/src/extrapolate_history.py
+++ src/day9/src/extrapolate_history.py
@@ -1,4 +1,4 @@
-def extrapolate_value_from_history_end(history):
+def extrapolate_value_from_history(history, isEnd=True):
     new_hist = []
     all_zeroes = True
     for i in range(1, len(history)):
@@ -7,43 +7,26 @@ def extrapolate_value_from_history_end(history):
             all_zeroes = False
 
     if all_zeroes:
-        return history[-1]
+        return history[-1] if isEnd else history[0]
     else:
-        return history[-1] + extrapolate_value_from_history_end(new_hist)
+        return history[-1] + extrapolate_value_from_history(new_hist) if isEnd \
+            else history[0] - extrapolate_value_from_history(new_hist, False)
 
-def solve_part1(input_lines):
+
+def solve_part1_and_part2(input_lines):
     extrapolated_values_begin = 0
-    for line in input_lines:
-        extrapolated_values_begin += extrapolate_value_from_history_end([int(n) for n in line.split(" ")])
-
-    return extrapolated_values_begin
-
-def extrapolate_value_from_history_beginning(history):
-    new_hist = []
-    all_zeroes = True
-    for i in range(1, len(history)):
-        new_hist.append(history[i]-history[i-1])
-        if all_zeroes and new_hist[i-1] != 0:
-            all_zeroes = False
-
-    if all_zeroes:
-        return history[0]
-    else:
-        return history[0] - extrapolate_value_from_history_beginning(new_hist)
-
-def solve_part2(input_lines):
     extrapolated_values_end = 0
     for line in input_lines:
-        extrapolated_values_end += extrapolate_value_from_history_beginning([int(n) for n in line.split(" ")])
+        extrapolated_values_begin += extrapolate_value_from_history([int(n) for n in line.split(" ")])
+        extrapolated_values_end += extrapolate_value_from_history([int(n) for n in line.split(" ")], False)
 
-    return extrapolated_values_end
+    return extrapolated_values_begin, extrapolated_values_end
 
 
 if __name__ == "__main__":
     input = open(r"day9.txt")
-    print("Day 9 Part 1:", solve_part1(input.readlines()))
+    part1, part2 = solve_part1_and_part2(input.readlines())
     input.close()
 
-    input = open(r"day9.txt")
-    print("Day 6 Part 2:", solve_part2(input.readlines()))
-    input.close()
+    print("Day 9 Part 1:", part1)
+    print("Day 9 Part 2:", part2)
--- src/day9/test/test_extrapolate_history.py
+++ src/day9/test/test_extrapolate_history.py
@@ -1,17 +1,17 @@
 import unittest
 
-from day9.src.extrapolate_history import solve_part1, solve_part2
+from day9.src.extrapolate_history import solve_part1_and_part2
 
 
 class Test_Extrapolate_History(unittest.TestCase):
     def test_extrapolate_value_from_history_beginning(self):
         input_lines = open(r"day9Test.txt")
-        extrapolated_values_begin = solve_part1(input_lines)
+        extrapolated_values_begin = solve_part1_and_part2(input_lines)[0]
         self.assertEqual(extrapolated_values_begin, 114, "Should be 114")
 
     def test_extrapolate_value_from_history_end(self):
         input_lines = open(r"day9Test.txt")
-        extrapolated_values_end = solve_part2(input_lines)
+        extrapolated_values_end = solve_part1_and_part2(input_lines)[1]
         self.assertEqual(extrapolated_values_end, 2, "Should be 2")
 
 
